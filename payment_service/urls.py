261d47dd73a4347dcf7bf005ea2fb5418226d2f8
RuslanTopal <baranotik15@gmail.com>
2025-05-19 12:08:43 +0000

    same changes

--- payment_service/urls.py
+++ payment_service/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PaymentViewSet
+from payment_service.views import PaymentViewSet
+
 
 router = DefaultRouter()
 router.register(r'payments', PaymentViewSet, basename='payment')
