7fbd3d436a9c6c1dbb0e17c5b70f771673d86750
Andrii <yurkivandriy02@gmail.com>
2025-05-19 14:12:27 +0300

    Implement the serializer & views for all the endpoints

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookReadSerializer, BookCreateSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return BookReadSerializer
+        return BookCreateSerializer
