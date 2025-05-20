1cf663e8bd5775f8f3e0aef84b6134b2b7487f50
Andrii <yurkivandriy02@gmail.com>
2025-05-20 19:38:39 +0300

    add functionality for send message to telegram

--- borrowings/tasks.py
+++ borrowings/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 from django.utils import timezone
 from borrowings.models import Borrowing
+from telegram_bot.telegram import send_telegram_message
 
 
 @shared_task
@@ -12,13 +13,14 @@ def check_overdue_borrowings():
     ).select_related("user", "book")
 
     if not overdue.exists():
-        print("No borrowings overdue today!")
+        send_telegram_message("No borrowings overdue today!")
         return
 
     for obj in overdue:
-        print(
-            f"📚 Overdue!\n"
-            f"Email: {obj.user.email}\n"
-            f"Book: {obj.book.title}\n"
-            f"Expected: {obj.expected_return_date}"
+        message = (
+            f"📚 <b>Overdue Borrowing</b>\n"
+            f"👤 User: {obj.user.email}\n"
+            f"📖 Book: {obj.book.title}\n"
+            f"📅 Expected Return: {obj.expected_return_date.strftime('%Y-%m-%d %H:%M')}"
         )
+        send_telegram_message(message)
